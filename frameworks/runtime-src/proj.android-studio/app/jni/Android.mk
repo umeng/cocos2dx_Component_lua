7fad7c6be9819a58a2568c6a6e928fb988c9881e
wf90163 <wf90163@alibaba-inc.com>
2017-09-23 16:37:53 +0800

    fix
    Former-commit-id: 8610ef1827040568cd62dfed50ee412d02fabd6f

--- frameworks/runtime-src/proj.android-studio/app/jni/Android.mk
+++ frameworks/runtime-src/proj.android-studio/app/jni/Android.mk
@@ -2,6 +2,41 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_MODULE := libweibosdkcore
+LOCAL_SRC_FILES := prebuild/armeabi/libweibosdkcore.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libweibosdkcorev8
+LOCAL_SRC_FILES := prebuild/arm64-v8a/libweibosdkcore.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libweibosdkcorev7
+LOCAL_SRC_FILES := prebuild/armeabi-v7a/libweibosdkcore.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libweibosdkcoremips
+LOCAL_SRC_FILES := prebuild/mips/libweibosdkcore.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libweibosdkcoremips64
+LOCAL_SRC_FILES := prebuild/mips64/libweibosdkcore.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libweibosdkcorex86
+LOCAL_SRC_FILES := prebuild/x86/libweibosdkcore.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libweibosdkcorex64
+LOCAL_SRC_FILES := prebuild/x86_64/libweibosdkcore.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+
 LOCAL_MODULE := cocos2dlua_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dlua
